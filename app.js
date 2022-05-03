0f8443c312cc90730329d32551c0463c1f08c862
Khalid Aboulgheit <khalidaboulgheit1@gmail.com>
2022-05-03 23:48:25 +0200

    start basic express server

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express()
+const port = 3000
+
+app.get('/',(req, res) =>{
+    res.status(200).send(`<h1>task manager app</h1>`)
+})
+
+app.all('*', (req,res) => {
+    res.status(404).send(`<h1> Error 404 Not found</h1> <a href = '/'>Back home page</a>`);
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}...`
+    )
+})
