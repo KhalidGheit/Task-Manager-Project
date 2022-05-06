9128797b8acb6a1c7241545b691f7863371eab9a
Khalid Aboulgheit <khalidaboulgheit1@gmail.com>
2022-05-06 23:51:54 +0200

    handel wrong url

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express()
+app.use(express.static('./public'));
 const port = 3000
 const connectDB = require('./DB/connection');
 require('dotenv').config()
 const tasks = require('./routes/tasks')
 app.use(express.json());
 app.use('/api/v1/tasks', tasks)
-app.use(express.static('./public'))
+app.use('*', (req,res) =>
+{
+    res.send(`<h1>Error 404 not found</h1><a href="/"> go to tasks page</a>`);
+})
 const start = async () => {
 try {
     await connectDB(process.env.MONGO_URL)
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,29 +1,30 @@
-const task = require('../modules/task');
+const Task = require('../modules/task');
 
 const getAllTasks = async (req, res) => {
     try {
-        const tasks = await task.find();
-        res.status(200).json(tasks);
+        const tasks = await Task.find({});
+        res.status(200).json({ tasks });
     } catch (error) {
         res.json({msg: error.message})
     }
 }
 const creatTask = async (req, res) => {
     try {
-        const tasks = await task.create(req.body)
-        res.status(200).json(tasks);
+        const task = await Task.create(req.body)
+        res.status(200).json(task);
     } catch (error) {
         res.status(500).json({ msg: error.message });
     }
 }
 const getTask = async (req, res) => {
     try {
-        const tasks = await task.findOne({_id : req.params.id});
-        if (!tasks)
+        const taskID = req.params.id;
+        const task = await Task.findOne({_id : taskID});
+        if (!task)
         {
-            return res.status(404).json({ msg:`no such task with id : ${req.params.id}` });
+            return res.status(404).json({ msg:`no such task with id : ${taskID}` });
         }
-        res.status(200).json(tasks);
+        res.status(200).json(task);
     } catch (error) {
         res.status(500).json({ msg: error});
     }
@@ -31,12 +32,13 @@ const getTask = async (req, res) => {
 }
 const deleteTask = async (req, res) => {
     try {
-        const tasks = await task.findOneAndDelete({_id : req.params.id}) 
-        if (!tasks)
+        const taskID = req.params.id;
+        const task = await Task.findOneAndDelete({_id : taskID}) 
+        if (!task)
         {
-            return res.status(404).json({ msg:`no such task with id : ${req.params.id}` });
+            return res.status(404).json({ msg:`no such task with id : ${taskID}` });
         }
-        res.status(200).json(tasks);
+        res.status(200).json(task);
     } catch (error) {
         res.status(500).json({ msg: error});
     }
@@ -44,15 +46,16 @@ const deleteTask = async (req, res) => {
 
 const updateTask = async (req, res) => {
     try {
-        const tasks = await task.findOneAndUpdate({_id: req.params.id}, req.body, {
+        const taskID = req.params.id;
+        const task = await Task.findOneAndUpdate({_id: taskID}, req.body, {
             new : true,
             runValidators : true
         })
-        if (!tasks)
+        if (!task)
         {
-            return res.status(404).json({ msg:`no such task with id : ${req.params.id}` });
+            return res.status(404).json({ msg:`no such task with id : ${taskID}` });
         }
-        res.status(200).json(tasks);
+        res.status(200).json(task);
     } catch (error) {
         res.status(500).json({ msg: error });
 
